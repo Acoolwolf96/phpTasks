f708bfcb994ecd671ec54cf5f34d2b9a3a26f391
Nelson Njoku <113510892+Acoolwolf96@users.noreply.github.com>
2024-01-28 16:27:16 +0200

    slight changes

--- src/nelson_tasks/ex1.php
+++ src/nelson_tasks/ex1.php
@@ -12,7 +12,7 @@
 </h3>
 <?php
     // Use backslashes to escape double quotes or use single quotes
-    echo 'Hello world! My name is Nelson';
+    echo 'Hello world! My name is "Nelson"';
 ?>
 
 <h3>3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an (heading 4) element.</h3>
